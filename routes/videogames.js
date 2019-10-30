e2e0127abb96012b0c5d07bd160ea6f9f713b484
KADIM Ahmed <akadim70@gmail.com>
2019-10-30 15:19:44 +0100

    Adding the Add/Edit of Videogame API

--- routes/videogames.js
+++ routes/videogames.js
@@ -29,6 +29,45 @@ router.get('/', function(req, res, next) {
   return res.send(Object.values(videogames));
 });
 
+/* GET a specific platform. */
+router.get('/:id', function(req, res, next) {
+  let id = parseInt(req.params.id);
+
+  videogames.forEach( (videogame) => {
+     if(videogame.id === id) {
+        return res.end(JSON.stringify(videogame));
+     }
+ });  
+});
+
+/* ADD a platform. */
+router.post('/', function(req, res, next) {
+  
+  videogames.push({
+      id: ( (videogames.length === 0) ? 0 : parseInt(videogames[videogames.length-1].id) + 1),
+      name: req.body.name,
+      platform: req.body.platform
+  });
+
+  return res.status(200).send({"message" : "added"});
+});
+
+/* UPDATE a platform. */
+router.put('/', function(req, res, next) {
+  
+  videogames = videogames.map( (videogame) => {
+     if(videogame.id === parseInt(req.body.id)) {
+
+      videogame.name = req.body.name;
+      videogame.platform = req.body.platform;
+     }
+
+     return videogame;
+  }); 
+
+  return res.status(200).send({"message" : "updated"});
+});
+
 /* Delete a videogame */
 router.delete('/:id', function(req, res, next) {
   let id = parseInt(req.params.id);
